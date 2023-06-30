42ded6d1f8b432ee02f39b82036c53f82e85b84f
gaoyang <xz@lgysh.cn>
2023-06-30 18:48:36 +0800

    add open foleder and show message command.

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,35 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  vscode.window.showInformationMessage('Example extension activate!')
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "example-extension" is now active!');
+  const commands = [
+    vscode.commands.registerCommand('example-extension.showInformationMessage', () => {
+      vscode.window.showInformationMessage('Hello World from example-extension!')
+    }),
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('example-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from example-extension!');
-	});
+    vscode.commands.registerCommand('example-extension.openFolder', () => {
+      vscode.window
+        .showOpenDialog({
+          canSelectFiles: false,
+          canSelectFolders: true,
+          canSelectMany: false
+        })
+        .then(folderUri => {
+          if (folderUri && folderUri[0]) {
+            vscode.commands.executeCommand('vscode.openFolder', folderUri[0])
+          }
+        })
+    }),
 
-	context.subscriptions.push(disposable);
+    vscode.commands.registerCommand('example-extension.showInformationMessage', () => {
+      vscode.window.showInformationMessage('Hello World from example-extension!')
+    }),
+  ]
+
+  commands.forEach(command => context.subscriptions.push(command))
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  vscode.window.showInformationMessage('Example extension deactivate.')
+}
